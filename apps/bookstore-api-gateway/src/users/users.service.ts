8f236aac6174b472dace36df0762b0cbc5d9e516
Shashwat <sshashwatssingh@gmail.com>
2025-07-11 20:02:07 +0530

    added modularity and scalibilty to micoservices

--- apps/bookstore-api-gateway/src/users/users.service.ts
+++ apps/bookstore-api-gateway/src/users/users.service.ts
@@ -1,9 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { USERS_CLIENT } from './constants';
 
 @Injectable()
 export class UsersService {
-    constructor (@Inject('USERS_CLIENT') private usersClient: ClientProxy) {}
+    constructor (@Inject(USERS_CLIENT) private usersClient: ClientProxy) {}
 
     findAll() {
         return this.usersClient.send('users.findAll', {});
